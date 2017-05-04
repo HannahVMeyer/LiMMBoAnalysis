1db1f3c3cec10fde9ea939b1071585a31e4aa274
HannahVMeyer <hannah.v.meyer@googlemail.com>
2017-05-04 16:37:22 +0100

    Change plot for calibration of LM/LMM

--- EvaluateLiMMBo/EvaluateCalibration.R
+++ EvaluateLiMMBo/EvaluateCalibration.R
@@ -1,7 +1,12 @@
+###############################
+### Libraries and Functions ###
+###############################
+
 library("data.table")
 library("ggplot2")
 library("wesanderson")
 library("dplyr")
+library("reshape2") # melt, acast
 
 moonrise <- c(wes_palette(n=4, name="Moonrise1"), 
               wes_palette(n=4, name="Moonrise2"))
@@ -42,15 +47,25 @@ calibrationAcrossChromosomes <- function(gwas, directory, estimateString, K,
     return(list(cal_pvalues, length(pvalues), missing))
 }
 
+############
+### data ### 
+############
+
 N=1000
 NrSNPs=20
 genVariance <- c(0.2, 0.5, 0.8)
 model <- "noiseBgOnlygeneticBgOnly"
-kinship <- c("unrelatedEU_popstructure", "unrelatedEU_nopopstructure", 
-             "relatedEU_nopopstructure")
-dirroot <- "/homes/hannah/GWAS/data/LiMMBo/Calibration/samples1000_NrSNP20_Cg"
-resultdir <- "/homes/hannah/GWAS/data/LiMMBo/Calibration"
+kinship <- c("unrelated_popstructure", "unrelated_nopopstructure", 
+             "related_nopopstructure")
+dirroot <- "~/GWAS/data/LiMMBo/Calibration/samples1000_NrSNP20_Cg"
+resultdir <- "~/GWAS/data/LiMMBo/Calibration"
 Traits <- seq(10,100,10)
+
+alpha <- c(5e-5, 5e-6, 5e-7, 5e-8)
+
+################
+### analysis ### 
+################
 
 calibrationH2 <- lapply(genVariance, function(h2) {
     calibrationKinship <- lapply(kinship, function(K) {
@@ -96,9 +111,9 @@ calibrationH2 <- lapply(genVariance, function(h2) {
                               collapse=", ")
                 }
                 tmp_closedForm <- data.frame(alpha=alpha, 
-                                             lm_pc=rep(NA, length(alpha)), 
-                                             lm=rep(NA, length(alpha)), 
-                                             lmm=cal_closedForm[[1]], 
+                                             LM=rep(NA, length(alpha)), 
+                                             sLM=rep(NA, length(alpha)), 
+                                             LMM=cal_closedForm[[1]], 
                                              P=rep(P, length(alpha)), 
                                              h2=rep(h2, length(alpha)), 
                                              model=rep(model, length(alpha)),  
@@ -123,67 +138,103 @@ calibrationH2 <- lapply(genVariance, function(h2) {
 calibrationBgOnly <- do.call(rbind, calibrationH2)
 calibrationBgOnly$alpha <- factor(calibrationBgOnly$alpha, 
                                   level=c(5e-5, 5e-6, 5e-7, 5e-8))
-saveRDS(calibrationBgOnly, paste(resultdir, "/calibrationBGOnly.rds", sep=""))
+calibrationBgOnly$kinship <- factor(calibrationBgOnly$kinship, 
+                                  level=kinship)
+saveRDS(calibrationBgOnly, paste(resultdir, "/calibrationBGOnly_woRML.rds", sep=""))
+calibrationBgOnly$LMM [calibrationBgOnly$LMM == 0 ] <- 5e-8 
+
+axistext <- 16
+axistitle <- 16
+legendtext <- 16
+legendtitle <- 16
 
 pdf(file=paste(resultdir, "/calibrationBGOnly.pdf", sep=""),  height=12, 
     width=12)
-p <- ggplot(filter(calibrationBgOnly, estimate == "LiMMBo"), 
+p <- ggplot(filter(calibrationBgOnly, estimate == "LiMMBo",
+                   alpha %in% c(5e-5, 5e-8)), 
             aes(x=as.factor(P), y=-log10(lm)))
-p + geom_bar(stat = "identity", position="dodge",aes(fill=alpha)) + 
+p + geom_bar(stat = "identity", position="dodge",aes(fill=alpha), alpha=0.8) + 
     facet_grid(kinship ~ h2) +
     scale_fill_manual(values = moonrise[5:8]) +
     labs(x = "Number of traits", y = expression(-log[10](FDR)), title="LM") +
  	geom_hline(yintercept = -log10(alpha[1]), colour = moonrise[5]) +
- 	geom_hline(yintercept = -log10(alpha[2]), colour = moonrise[6]) +
- 	geom_hline(yintercept = -log10(alpha[3]), colour = moonrise[7]) +
- 	geom_hline(yintercept = -log10(alpha[4]), colour = moonrise[8]) +
+ 	geom_hline(yintercept = -log10(alpha[4]), colour = moonrise[6]) +
     theme_bw() +
     theme(strip.text.x = element_text(size = 8))
 
-p <- ggplot(filter(calibrationBgOnly, estimate =="estimateVD"), 
+p <- ggplot(filter(calibrationBgOnly, estimate =="LiMMBo",
+                   alpha %in% c(5e-5, 5e-8)), 
             aes(x=as.factor(P), y=-log10(lm_pc)))
-p + geom_bar(stat = "identity", position="dodge",aes(fill=alpha)) +
+p + geom_bar(stat = "identity", position="dodge",aes(fill=alpha), alpha=0.8) +
     facet_grid(kinship ~ h2) +
     scale_fill_manual(values = moonrise[5:8]) +
     labs(x = "Number of traits", y = expression(-log[10](FDR)), 
          title="LM with PCs") +
  	geom_hline(yintercept = -log10(alpha[1]), colour = moonrise[5]) +
- 	geom_hline(yintercept = -log10(alpha[2]), colour = moonrise[6]) +
- 	geom_hline(yintercept = -log10(alpha[3]), colour = moonrise[7]) +
- 	geom_hline(yintercept = -log10(alpha[4]), colour = moonrise[8]) +
+ 	geom_hline(yintercept = -log10(alpha[4]), colour = moonrise[6]) +
     theme_bw() +
     theme(strip.text.x = element_text(size = 8))
 
-p <- ggplot(filter(calibrationBgOnly, estimate =="estimateVD"), 
+p <- ggplot(filter(calibrationBgOnly, estimate =="LiMMBo",
+                   alpha %in% c(5e-5, 5e-8)), 
             aes(x=as.factor(P), y=-log10(lmm)))
-p + geom_bar(stat = "identity", position="dodge",aes(fill=alpha)) +
+p + geom_bar(stat = "identity", position="dodge",aes(fill=alpha), alpha=0.8) +
     facet_grid(kinship ~ h2) +
     scale_fill_manual(values = moonrise[5:8]) +
     labs(x = "Number of traits", y = expression(-log[10](FDR)), title="LMM") +
  	geom_hline(yintercept = -log10(alpha[1]), colour = moonrise[5]) +
- 	geom_hline(yintercept = -log10(alpha[2]), colour = moonrise[6]) +
- 	geom_hline(yintercept = -log10(alpha[3]), colour = moonrise[7]) +
- 	geom_hline(yintercept = -log10(alpha[4]), colour = moonrise[8]) +
+ 	geom_hline(yintercept = -log10(alpha[4]), colour = moonrise[6]) +
     theme_bw() +
     theme(strip.text.x = element_text(size = 8))
+dev.off()
 
+calibrationBgOnly.m <- melt(calibrationBgOnly[,1:9], 
+                            measure.vars=c("LM", "LMM", "sLM"),
+                            value.name="fdr",
+                            variable.name="analysis")
+col <- wes_palette(n=8, name="Moonrise2", type = 'continuous')
+
+pdf(file=paste(resultdir, "/calibrationBGOnly_combined.pdf", sep=""), 
+    height=12, width=12)
+p <- ggplot(filter(calibrationBgOnly.m, estimate == "LiMMBo",
+                   alpha %in% c(5e-8),
+                   analysis != "sLM"), 
+            aes(x=as.factor(P), y=-log10(fdr)))
+
+p + geom_bar(stat = "identity", position="dodge", aes(fill=analysis), 
+             alpha=0.8) + 
+    facet_grid(kinship ~ h2) +
+    scale_fill_manual(values = col[c(1,5)], name="Model") +
+    labs(x = "Number of traits", y = expression(-log[10](FDR))) +
+    geom_hline(yintercept = -log10(alpha[4]), colour = moonrise[6]) +
+    theme_bw() +
+    theme(strip.text.x = element_text(size = 8))
 dev.off()
 
 pdf(file=paste(resultdir, "/calibrationBGOnly_closedForm.pdf", sep=""), 
     height=12, width=12)
-p <- ggplot(filter(calibrationBgOnly, P <= 30), aes(x=as.factor(P), 
+p <- ggplot(filter(calibrationBgOnly, P <= 30, !is.na(lmm), 
+                   alpha %in% c(5e-5, 5e-8)), aes(x=as.factor(P), 
                                                     y=-log10(lmm)))
-p + geom_bar(stat = "identity", position="dodge",
-             aes(fill=alpha, alpha=estimate)) +
-    facet_grid(kinship ~ h2) +
-    scale_fill_manual(values = moonrise[c(5:8)]) +
-	scale_alpha_manual(values = c(0.4, 0.8)) + 
-    labs(x = "Number of traits", y = expression(-log[11](FDR)), title="LMM") +
+p + geom_boxplot(position=position_dodge(width=1), aes(fill=alpha, alpha=estimate)) +
+    #facet_grid(kinship ~ h2) +
+    scale_fill_manual(values = moonrise[c(5:8)], name="FDR threshold") +
+	scale_alpha_manual(values = c(0.4, 0.8), name="VD method") + 
+    labs(x = "Number of traits", y = expression(-log[10](FDR))) +
     geom_hline(yintercept = -log10(alpha[1]), colour = moonrise[5]) +
-    geom_hline(yintercept = -log10(alpha[2]), colour = moonrise[6]) +
-    geom_hline(yintercept = -log10(alpha[3]), colour = moonrise[7]) +
-    geom_hline(yintercept = -log10(alpha[4]), colour = moonrise[8]) +
-    ylim(c(0,10))+
+    geom_hline(yintercept = -log10(alpha[4]), colour = moonrise[6]) +
+    ylim(c(4,8)) +
+    guides(alpha=guide_legend(override.aes=list(fill=hcl(c(15,195),
+                                                         100, 0, 
+                                                         alpha=c(0.4,0.8)),
+                                                colour=NA))) +
     theme_bw() +
-    theme(strip.text.x = element_text(size = 8))
+    theme(axis.text.x = element_text(colour="black",size=axistext,angle=0,hjust=.5,vjust=.5,face="plain"),
+          axis.text.y = element_text(colour="black",size=axistext,angle=0,hjust=0.5,vjust=0.5,face="plain"),  
+          axis.title.x = element_text(colour="black",size=axistitle,angle=0,hjust=.5,vjust=0,face="plain"),
+          axis.title.y = element_text(colour="black",size=axistitle,angle=90,hjust=.5,vjust=.5,face="plain"),
+          legend.text = element_text(colour="black",size=legendtext,angle=0,hjust=1,vjust=0,face="plain"),
+          legend.title= element_text(colour="black",size=legendtitle,angle=0,hjust=1,vjust=0,face="plain"),
+          legend.key = element_rect(colour = NA)) 
 dev.off()
+
