977c565121d83720ab9320847c5ca27cb94554be
Hannah <hannah.v.meyer@gmail.com>
2017-05-05 14:03:05 +0100

    Return pvalues for analysis of related samples

--- EvaluateLiMMBo/EvaluateCalibration.R
+++ EvaluateLiMMBo/EvaluateCalibration.R
@@ -2,7 +2,12 @@ library("data.table")
 library("ggplot2")
 library("wesanderson")
 library("dplyr")
+library("plyr")
 
+
+rmNulls <- function(compList) {
+                nonNulls <- compList[!sapply(compList, is.null)]
+}
 moonrise <- c(wes_palette(n=4, name="Moonrise1"), 
               wes_palette(n=4, name="Moonrise2"))
 
@@ -39,7 +44,12 @@ calibrationAcrossChromosomes <- function(gwas, directory, estimateString, K,
     missing <- unlist(sapply(gwasdata, function(p) p[[2]]))
 
     cal_pvalues <- calibration(pvalues)
-    return(list(cal_pvalues, length(pvalues), missing))
+    if (K == "relatedEU_nopopstructure" && !grepl("closed", estimateString)) {
+        return(list(cal_pvalues, length(pvalues), missing, pvalues))
+    } else {
+        return(list(cal_pvalues, length(pvalues), missing, NULL))
+    
+    }
 }
 
 N=1000
@@ -63,31 +73,37 @@ calibrationH2 <- lapply(genVariance, function(h2) {
             directory=paste(dirroot, h2, "_model", model, "/", K, "/nrtraits", 
                             P, "/estimateVD/nrtraits_samples", sampling, 
                             sep="")
-            cal <- lapply(c("lm_mt_pcs", "lm_mt", "lmm_mt"), 
+            cal <- lapply(c("lm_mt_pcs", "lmm_mt"), 
                           calibrationAcrossChromosomes, directory=directory,  
                           estimateString="", K=K, P=P)
             missing <- sapply(cal, function(m) {
                 if (all(is.na(m[[3]]))) return(NA)
                 else paste(m[[3]][!is.na(m[[3]])], collapse=", ")
             })
-            tmp <- data.frame(alpha=alpha, lm_pc=cal[[1]][[1]], 
-                              lm=cal[[2]][[1]], lmm=cal[[3]][[1]],
+            tmp_summary <- data.frame(alpha=alpha, lm=cal[[1]][[1]], 
+                              lmm=cal[[2]][[1]],
                               P=rep(P, length(alpha)), 
                               h2=rep(h2, length(alpha)),
                               model=rep(model, length(alpha)),  
                               kinship=rep(K, length(alpha)), 
-                              estimate=rep("estimateVD", length(alpha)), 
-                              missing_lm_pc=rep(missing[1], length(alpha)), 
-                              missing_lm=rep(missing[2], length(alpha)), 
-                              missing_lmm=rep(missing[3], length(alpha)))
+                              estimate=rep("LiMMBo", length(alpha)), 
+                              missing_lm=rep(missing[1], length(alpha)), 
+                              missing_lmm=rep(missing[2], length(alpha)),
+                              stringsAsFactors=FALSE)
+
+            tmp_pvalues <- data.frame(lm=cal[[1]][[4]], lmm=cal[[2]][[4]])
+            if (K == "relatedEU_nopopstructure") {
+                names(tmp_pvalues) <- paste(names(tmp_pvalues), "_", P, "_", h2, 
+                                        "_", K, sep="")
+            }
             
-            if (P <= 30 && FALSE) {
+            if (P <= 30 ) {
                 directory=paste(dirroot, h2, "_model", model, "/", K, 
                                 "/nrtraits", P, "/closedForm",  sep="")
                 cal_closedForm <- 
                     calibrationAcrossChromosomes("lmm_mt", directory=directory,
                                                  estimateString="_closedForm", 
-                                                 K=K)
+                                                 K=K, P=P)
                 if (all(is.na(cal_closedForm[[3]]))) {
                     missing <- NA
                 } else {
@@ -96,38 +112,51 @@ calibrationH2 <- lapply(genVariance, function(h2) {
                               collapse=", ")
                 }
                 tmp_closedForm <- data.frame(alpha=alpha, 
-                                             lm_pc=rep(NA, length(alpha)), 
                                              lm=rep(NA, length(alpha)), 
                                              lmm=cal_closedForm[[1]], 
                                              P=rep(P, length(alpha)), 
                                              h2=rep(h2, length(alpha)), 
                                              model=rep(model, length(alpha)),  
                                              kinship=rep(K, length(alpha)), 
-                                             estimate=rep("closedForm", 
+                                             estimate=rep("RML", 
                                                           length(alpha)), 
-                                             missing_lm_pc=rep(missing[1], 
+                                             missing_lm=rep(missing[1], 
                                                                length(alpha)), 
-                                             missing_lm=rep(missing[2], 
-                                                            length(alpha)), 
                                              missing_lmm=rep(missing[3], 
-                                                             length(alpha)))
-                tmp <- rbind(tmp, tmp_closedForm)
+                                                             length(alpha)),
+                                            stringsAsFactors=FALSE)
+                tmp_summary <- rbind(tmp_summary, tmp_closedForm)
             } 
+           tmp <- list(tmp_summary, tmp_pvalues)
+           #names(tmp) <- paste("Traits", Traits, sep="")
            return(tmp)
         })
-        cal <- do.call(rbind, calibrationTraits)
+        cal_summary <- ldply(calibrationTraits, function(x) x[[1]])
+        cal_pvalues <- do.call(cbind, lapply(calibrationTraits, 
+                                             function(x) x[[2]]))
+        tmp <- list(cal_summary, cal_pvalues)
+        #names(tmp) <- kinship
+        return(tmp)
     })
-    cal <- do.call(rbind, calibrationKinship)
+    cal_summary <- ldply(calibrationKinship, function(x) x[[1]])
+    cal_pvalues <- lapply(calibrationKinship, function(x) x[[2]])
+    cal_pvalues <- cal_pvalues[sapply(cal_pvalues, function(x) dim(x)[1] != 0)]
+    cal_pvalues <- do.call(cbind, cal_pvalues)
+    tmp <- list(cal_summary, cal_pvalues)
+    #names(tmp) <- paste("h2_", genVariance, sep="")
+    return(tmp)
 })
 
-calibrationBgOnly <- do.call(rbind, calibrationH2)
-calibrationBgOnly$alpha <- factor(calibrationBgOnly$alpha, 
+calibrationSummary <- ldply(calibrationH2, function(x) x[[1]])
+calibrationPvalues <- do.call(cbind, lapply(calibrationH2, function(x) x[[2]]))
+calibrationSummary$alpha <- factor(calibrationSummary$alpha, 
                                   level=c(5e-5, 5e-6, 5e-7, 5e-8))
-saveRDS(calibrationBgOnly, paste(resultdir, "/calibrationBGOnly.rds", sep=""))
+saveRDS(calibrationSummary, paste(resultdir, "/calibrationSummary.rds", sep=""))
+saveRDS(calibrationPvalues, paste(resultdir, "/calibrationPvalues.rds", sep=""))
 
-pdf(file=paste(resultdir, "/calibrationBGOnly.pdf", sep=""),  height=12, 
+pdf(file=paste(resultdir, "/calibrationSummary.pdf", sep=""),  height=12, 
     width=12)
-p <- ggplot(filter(calibrationBgOnly, estimate == "LiMMBo"), 
+p <- ggplot(filter(calibrationSummary, estimate == "LiMMBo"), 
             aes(x=as.factor(P), y=-log10(lm)))
 p + geom_bar(stat = "identity", position="dodge",aes(fill=alpha)) + 
     facet_grid(kinship ~ h2) +
@@ -140,7 +169,7 @@ p + geom_bar(stat = "identity", position="dodge",aes(fill=alpha)) +
     theme_bw() +
     theme(strip.text.x = element_text(size = 8))
 
-p <- ggplot(filter(calibrationBgOnly, estimate =="estimateVD"), 
+p <- ggplot(filter(calibrationSummary, estimate =="LiMMBo"), 
             aes(x=as.factor(P), y=-log10(lm_pc)))
 p + geom_bar(stat = "identity", position="dodge",aes(fill=alpha)) +
     facet_grid(kinship ~ h2) +
@@ -154,7 +183,7 @@ p + geom_bar(stat = "identity", position="dodge",aes(fill=alpha)) +
     theme_bw() +
     theme(strip.text.x = element_text(size = 8))
 
-p <- ggplot(filter(calibrationBgOnly, estimate =="estimateVD"), 
+p <- ggplot(filter(calibrationSummary, estimate =="LiMMBo"), 
             aes(x=as.factor(P), y=-log10(lmm)))
 p + geom_bar(stat = "identity", position="dodge",aes(fill=alpha)) +
     facet_grid(kinship ~ h2) +
@@ -169,9 +198,9 @@ p + geom_bar(stat = "identity", position="dodge",aes(fill=alpha)) +
 
 dev.off()
 
-pdf(file=paste(resultdir, "/calibrationBGOnly_closedForm.pdf", sep=""), 
+pdf(file=paste(resultdir, "/calibrationSummary_closedForm.pdf", sep=""), 
     height=12, width=12)
-p <- ggplot(filter(calibrationBgOnly, P <= 30), aes(x=as.factor(P), 
+p <- ggplot(filter(calibrationSummary, P <= 30), aes(x=as.factor(P), 
                                                     y=-log10(lmm)))
 p + geom_bar(stat = "identity", position="dodge",
              aes(fill=alpha, alpha=estimate)) +
